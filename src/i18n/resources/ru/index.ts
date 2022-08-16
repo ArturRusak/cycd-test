2912d9795ed8efa4f4dfe230fb9b5d754bd0ea01
ArturRusak <arturrusakk@gmail.com>
2022-08-16 18:40:55 +0300

    Updates

--- src/i18n/resources/ru/index.ts
+++
@@ -1,9 +0,0 @@
-import common from './common.json';
-import form from './form.json';
-
-const resources = {
-  common,
-  form,
-};
-
-export default resources;
--- src/types/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './Locale.enum';
-export * from './Organization';
-export * from './ThemeMode.enum';
-export * from './User';
